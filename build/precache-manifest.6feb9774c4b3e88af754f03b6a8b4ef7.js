8f0dbffcaca65c5c16af3f3ce1eee7a24dc1e047
Christian Mora <christianmora007@hotmail.com>
2020-08-03 16:03:08 -0400

    status

---
+++ build/precache-manifest.6feb9774c4b3e88af754f03b6a8b4ef7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a2aa65fb078b6fd6ab40b9c90bb022f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1abbc6c38f6cf36d2fa7",
+    "url": "/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "97a926a6ad9fbf148772",
+    "url": "/static/css/main.2c74e16c.chunk.css"
+  },
+  {
+    "revision": "1abbc6c38f6cf36d2fa7",
+    "url": "/static/js/2.673117fb.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/static/js/2.673117fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97a926a6ad9fbf148772",
+    "url": "/static/js/main.81d3d638.chunk.js"
+  },
+  {
+    "revision": "646a14845a0b0748253f",
+    "url": "/static/js/runtime-main.acb040d7.js"
+  },
+  {
+    "revision": "9072825662bab4ace88cb5b49f6061f4",
+    "url": "/static/media/Colorful-3D.90728256.jpg"
+  },
+  {
+    "revision": "f96beb9b00c4755c00d1f153943e0a16",
+    "url": "/static/media/background.f96beb9b.jpg"
+  },
+  {
+    "revision": "7e10da0b248f7882a203a95e19b171d4",
+    "url": "/static/media/hexagon1.7e10da0b.jpg"
+  }
+]);
